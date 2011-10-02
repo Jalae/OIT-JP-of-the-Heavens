b57ecc4ed330926415977a8f06da12b227eeda0b
Kehnin Dyer <kehnin@gmail.com>
2011-10-02 08:19:14 +0000

    added the Google Voice php Api files

--- WebFront/index.php
+++ WebFront/index.php
@@ -1,14 +1,12 @@
 <?php
-$gvnum = "";
 $acctname = "kehnin.dyer@oit.edu";
 $acctpass = "pinkballons";
-$options = array(
-		'return-info' => true,
-		'method' =>'post'
-		);
-$contents = file_get_contents('http://voice.google.com', $options);
+//TODO: recapta support incase google wants to cock block the auto login
 
-$galx = 0;
 
-print_r($result);
+
+
+
+
+
 ?>
