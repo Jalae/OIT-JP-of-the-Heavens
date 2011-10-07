871744100f613860a63002b8b6c077e3d7f8376f
unknown <kehnin.dyer@.students.cset.oit.edu>
2011-10-07 15:35:31 -0700

    barebones project for AVR Studio

---
+++ JPoth-avr-code/JPoth-avr-code/JPoth-avr-code.c
@@ -0,0 +1,23 @@
+/*
+ * JPoth_avr_code.c
+ *
+ * Created: 10/7/2011 2:28:09 PM
+ *  Author: kehnin.dyer
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
+
+
+PCINT0
+{
+	
+	
+}
