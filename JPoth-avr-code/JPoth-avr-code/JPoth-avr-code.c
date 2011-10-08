db6166dad3b90d4094b8c7d86a390a1633ee4658
unknown <kehnin.dyer@.students.cset.oit.edu>
2011-10-07 18:27:11 -0700

    Added interupt control to PORTB0. added more ignores for AVR studio.

--- JPoth-avr-code/JPoth-avr-code/JPoth-avr-code.c
+++ JPoth-avr-code/JPoth-avr-code/JPoth-avr-code.c
@@ -6,18 +6,41 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+ISR(PCINT1_vect)
+{
+	PORTB ^= 2;
+
+}
 
 int main(void)
 {
+	sei();
+	DDRB = 1<<PB1;
+	PORTB = 0xFF;
+
+	PCICR = 1<<PCIE1;
+	PCMSK1 = 1<<PCINT8;
+	//EIMSK = 1<<PCIE1;
+	
+	
+	
+	
+	
+	
     while(1)
     {
+		//if(PINB & 0x01)
+		//{
+			//PORTB |= 0x02;
+		//}			
+		//else
+		//{
+			//PORTB &= ~0x02;
+		//}			
         //TODO:: Please write your application code 
     }
 }
 
-
-PCINT0
-{
-	
-	
-}
