14d2b1af6096eea2d32c45f94037ccb7b00ad897
nolancheck <nolan.check@oit.edu>
2011-10-26 13:16:18 -0700

    Add lcd library (nothing there yet), get stuff working again

---
+++ JPoth-avr-code/JPoth-avr-code/lcd.c
@@ -0,0 +1,8 @@
+/*
+ * lcd.c
+ *
+ * Created: 10/26/2011 1:11:48 PM
+ *  Author: nolan.check
+ */ 
+
+#include "lcd.h"
---
+++ JPoth-avr-code/JPoth-avr-code/lcd.h
@@ -0,0 +1,16 @@
+/*
+ * lcd.h
+ *
+ * Created: 10/26/2011 1:11:25 PM
+ *  Author: nolan.check
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+
+
+
+#endif /* LCD_H_ */
